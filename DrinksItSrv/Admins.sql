6ccecb334b34c5b664800621181ff9e6f0b20cc6
Denny <Denny@dennyfilpc>
2016-11-08 23:06:39 +0100

    Migrated JSPs to Angular 2 on client side   * PB with report generation

--- DrinksItSrv/Admins.sql
+++
@@ -1,5 +0,0 @@
-USE DrinksItDB;
-INSERT into admins (userName, passwordHash) values ("DrinksItWSAdmin", "$2a$10$gFK6sBLv21clvPamex.qTuCUQycR.rhojDXQWrwnZvqYUzh4fa3eO");
-
-USE DrinksItDB;
-SELECT * FROM admins;
--- DrinksItSrv/src/main/java/ru/drinksit/auxillary/database/DatabaseException.java
+++
@@ -1,8 +0,0 @@
-package ru.drinksit.auxillary.database;
-
-public class DatabaseException extends Exception {
-
-	public DatabaseException(String message) {
-		super(message);
-	}
-}
